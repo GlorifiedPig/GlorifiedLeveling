92b87b0c29c5797c6f63386ea0a1f1875430b2c3
GlorifiedPig <killingpigs123@gmail.com>
2020-06-09 12:14:53 +0200

    import SQL stuff

---
+++ lua/glorifiedleveling/libraries/sv_glorifiedleveling_sql.lua
@@ -0,0 +1,59 @@
+
+GlorifiedLeveling.SQL = {}
+if GlorifiedLeveling.Config.SQL_TYPE == "mysqloo" then
+    require( "mysqloo" )
+
+    if mysqloo then
+        local connectionDetails = GlorifiedLeveling.Config.SQL_DETAILS
+        GlorifiedLeveling.SQL.Database = mysqloo.connect( connectionDetails[ "host" ], connectionDetails[ "user" ], connectionDetails[ "pass" ], connectionDetails[ "database" ], connectionDetails[ "port" ] )
+        function GlorifiedLeveling.SQL.Database:onConnected() print( "[GlorifiedLeveling] MySQL database connected, MySQLOO version " .. mysqloo.VERSION .. "." ) end
+        function GlorifiedLeveling.SQL.Database:onConnectionFailed( error ) print( "[GlorifiedLeveling] MySQL database connection failed:\n" .. error ) end
+        GlorifiedLeveling.SQL.Database:connect()
+    end
+end
+
+function GlorifiedLeveling.SQL.GetType()
+    if mysqloo and GlorifiedLeveling.Config.SQL_TYPE == "mysqloo" then return "mysqloo" end return "sqlite"
+end
+
+function GlorifiedLeveling.SQL.EscapeString( string )
+    if GlorifiedLeveling.SQL.Database then
+        return GlorifiedLeveling.SQL.Database:escape( string )
+    else
+        return sql.SQLStr( string, true )
+    end
+end
+
+GlorifiedLeveling.SQL.CachedErrors = {}
+function GlorifiedLeveling.SQL.ThrowError( error )
+    print( "[GlorifiedLeveling] An error occurred while trying to perform an SQL query:\n" .. error .. "\n" )
+    table.insert( GlorifiedLeveling.SQL.CachedErrors, error )
+end
+
+function GlorifiedLeveling.SQL.Query( sqlQuery, successFunc )
+    if GlorifiedLeveling.SQL.GetType() == "mysqloo" then
+        local query = GlorifiedLeveling.SQL.Database:query( sqlQuery )
+        if successFunc then
+            function query:onSuccess( queryData )
+                successFunc( queryData )
+            end
+        end
+        function query:onError( error ) GlorifiedLeveling.SQL.ThrowError( error ) end
+        query:start()
+    else
+        local queryData = sql.Query( sqlQuery )
+        if queryData == false then
+            if sql.LastError() then
+                GlorifiedLeveling.SQL.ThrowError( sql.LastError() )
+            end
+            return
+        end
+        if successFunc then successFunc( queryData ) end
+    end
+end
+
+concommand.Add( "glorifiedleveling_printsqlerrors", function( ply )
+    if ply == NULL or ply:IsSuperAdmin() then
+        PrintTable( GlorifiedLeveling.SQL.CachedErrors )
+    end
+end )
---
+++ lua/glorifiedleveling/sv_glorifiedleveling_config.lua
@@ -0,0 +1,11 @@
+
+--[[ SQL Configuration ]]--
+    GlorifiedLeveling.Config.SQL_TYPE = "sqlite" -- 'sqlite' or 'mysqloo'
+    GlorifiedLeveling.Config.SQL_DETAILS = {
+        [ "host" ] = "localhost",
+        [ "user" ] = "root",
+        [ "pass" ] = "",
+        [ "database" ] = "glorifiedleveling",
+        [ "port" ] = 3306
+    }
+--[[ End SQL Configuration ]]--
