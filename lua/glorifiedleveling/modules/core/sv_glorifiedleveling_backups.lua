59c0f743d3726aef06ecba2a48e799e9b6588511
GlorifiedPig <killingpigs123@gmail.com>
2020-06-19 20:29:11 +0200

    backups system

---
+++ lua/glorifiedleveling/modules/core/sv_glorifiedleveling_backups.lua
@@ -0,0 +1,68 @@
+
+if not GlorifiedLeveling.Config.BACKUPS_ENABLED then return end
+
+GlorifiedLeveling.LastBackup = cookie.GetNumber( "GlorifiedLeveling.LastBackup", os.time() )
+
+local function EnsureBackupDirectories()
+    if not file.Exists( GlorifiedLeveling.Config.BACKUPS_FOLDER_NAME, "DATA" ) then
+        file.CreateDir( GlorifiedLeveling.Config.BACKUPS_FOLDER_NAME )
+    end
+end
+
+local function DeleteOldBackups()
+    EnsureBackupDirectories()
+    local fileTbl = file.Find( GlorifiedLeveling.Config.BACKUPS_FOLDER_NAME .. "/*.txt", "DATA", "dateasc" )
+    local fileCount = table.Count( fileTbl )
+    if fileCount <= GlorifiedLeveling.Config.MAX_BACKUPS then return end
+    local deleteCount = fileCount - GlorifiedLeveling.Config.MAX_BACKUPS
+    for i = 1, deleteCount do
+        file.Delete( GlorifiedLeveling.Config.BACKUPS_FOLDER_NAME .. "/" .. fileTbl[i] )
+    end
+end
+
+function GlorifiedLeveling.CreateNewBackupFile()
+    EnsureBackupDirectories()
+    GlorifiedLeveling.SQL.Query( "SELECT * FROM `gl_players`", function( queryResult )
+        if not queryResult then return end
+        cookie.Set( "GlorifiedLeveling.LastBackup", os.time() )
+        GlorifiedLeveling.LastBackup = os.time()
+        file.Write( GlorifiedLeveling.Config.BACKUPS_FOLDER_NAME .. "/gl_backup_" .. os.time() .. ".txt", util.Compress( util.TableToJSON( queryResult ) ) )
+        DeleteOldBackups()
+    end )
+end
+
+function GlorifiedLeveling.ReadBackupFile( fileTime )
+    local readFile = file.Read( GlorifiedLeveling.Config.BACKUPS_FOLDER_NAME .. "/gl_backup_" .. fileTime .. ".txt" )
+    if readFile then
+        readFile = util.Decompress( readFile )
+        readFile = util.JSONToTable( readFile )
+    end
+    return readFile
+end
+
+function GlorifiedLeveling.LoadBackupFile( fileTime )
+    local readFile = GlorifiedLeveling.ReadBackupFile( fileTime )
+    if readFile then
+        GlorifiedLeveling.SQL.Query( "DELETE FROM `gl_players`", function()
+            for k, v in pairs( readFile ) do
+                GlorifiedLeveling.SQL.Query( "INSERT INTO `gl_players`( `SteamID64`, `Level`, `XP` ) VALUES ( '" .. v["SteamID64"] .. "', '" .. v["Level"] .. "', '" .. v["XP"] .. "' ) ")
+            end
+            print( "[GlorifiedLeveling] Loaded backup number " .. fileTime .. "." )
+        end )
+    end
+end
+
+hook.Add( "InitPostEntity", "GlorifiedLeveling.Backups.InitPostEntity", function()
+    timer.Create( "GlorifiedLeveling.BackupEnsureTimer", 60, 0, function()
+        -- Multiply by 3600 so that we can convert hours to seconds.
+        if os.time() >= GlorifiedLeveling.LastBackup + ( GlorifiedLeveling.Config.BACKUP_FREQUENCY * 3600 ) then
+            GlorifiedLeveling.CreateNewBackupFile()
+        end
+    end )
+end )
+
+concommand.Add( "GlorifiedLeveling_restorebackup", function( ply, args )
+    if ply == NULL or GlorifiedLeveling.HasPermission( ply, "GlorifiedLeveling_restorebackups" ) then
+        GlorifiedLeveling.LoadBackupFile( args[1] )
+    end
+end )
--- lua/glorifiedleveling/sv_glorifiedleveling_config.lua
+++ lua/glorifiedleveling/sv_glorifiedleveling_config.lua
@@ -9,3 +9,9 @@
         [ "port" ] = 3306
     }
 --[[ End SQL Configuration ]]--
+
+--[[ Backup System Configuration ]]--
+    GlorifiedLeveling.Config.BACKUPS_ENABLED = true -- Should the backup system be enabled?
+    GlorifiedLeveling.Config.BACKUPS_FOLDER_NAME = "glorifiedleveling_backups" -- What's the name of the folder in the data folder to store?
+    GlorifiedLeveling.Config.BACKUP_FREQUENCY = 2 -- How often in hours should the system backup?
+--[[ Backup System Configuration End ]]--
