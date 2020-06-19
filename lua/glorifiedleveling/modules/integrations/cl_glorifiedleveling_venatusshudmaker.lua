e1c0e5aa9f6af07c450d1fd091cb3fe843eca89e
GlorifiedPig <killingpigs123@gmail.com>
2020-06-19 20:14:12 +0200

    support for Venatuss' HUD Maker

---
+++ lua/glorifiedleveling/modules/integrations/cl_glorifiedleveling_venatusshudmaker.lua
@@ -0,0 +1,51 @@
+
+local HUD_MAKER = UI_MAKER.HUD_MAKER or {}
+
+UI_MAKER.FunctionsGamemodes["GlorifiedLeveling"] = {
+    func = function() return GlorifiedLeveling end,
+}
+
+HUD_MAKER.ListVariablesTxt["GlorifiedLeveling.Level"] = {
+    func = function( ply )
+        return GlorifiedLeveling and GlorifiedLeveling.GetPlayerLevel( ply )
+    end,
+    gm = "GlorifiedLeveling",
+    aliases = {
+        "%glorifiedleveling_level%"
+    },
+    matches = {
+        "glorifiedleveling",
+        "level"
+    }
+}
+
+HUD_MAKER.ListVariablesTxt["GlorifiedLeveling.XP"] = {
+    func = function( ply )
+        return GlorifiedLeveling and GlorifiedLeveling.GetPlayerXP( ply )
+    end,
+    gm = "GlorifiedLeveling",
+    aliases = {
+        "%glorifiedleveling_xp%"
+    },
+    matches = {
+        "glorifiedleveling",
+        "exp",
+        "xp"
+    }
+}
+
+HUD_MAKER.ListVariablesTxt["GlorifiedLeveling.MaxXP"] = {
+    func = function( ply )
+        return GlorifiedLeveling and GlorifiedLeveling.GetPlayerMaxXP( ply )
+    end,
+    gm = "GlorifiedLeveling",
+    aliases = {
+        "%glorifiedleveling_maxxp%"
+    },
+    matches = {
+        "glorifiedleveling",
+        "exp",
+        "xp",
+        "maxxp"
+    }
+}
