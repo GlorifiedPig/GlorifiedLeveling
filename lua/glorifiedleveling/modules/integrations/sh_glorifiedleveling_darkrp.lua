25ffbaff00520d0b39a4cee851e3b0a4e449e07b
GlorifiedPig <killingpigs123@gmail.com>
2020-06-20 00:12:23 +0200

    bugfixes

--- lua/glorifiedleveling/modules/integrations/sh_glorifiedleveling_darkrp.lua
+++ lua/glorifiedleveling/modules/integrations/sh_glorifiedleveling_darkrp.lua
@@ -4,7 +4,7 @@ if not GlorifiedLeveling.Config.SUPPORT_DARKRP then return end
 if SERVER then
     local function levelCheck( ply, ent )
         local entLevel = ent.level or ent.Level or ent.GlorifiedLeveling_Level
-        if entLevel and not GlorifiedLeveling.HasLevel( entLevel ) then
+        if entLevel and not GlorifiedLeveling.PlayerHasLevel( ply, entLevel ) then
             return false, GlorifiedLeveling.i18n.GetPhrase( "glLevelNotHighEnough" )
         end
     end
@@ -19,8 +19,58 @@ if SERVER then
         local jobFromNumber = RPExtraTeams[jobNumber]
         local jobLevel = jobFromNumber.level or jobFromNumber.Level or jobFromNumber.GlorifiedLeveling_Level
 
-        if jobLevel and not GlorifiedLeveling.HasLevel( jobLevel ) then
+        if jobLevel and not GlorifiedLeveling.PlayerHasLevel( ply, jobLevel ) then
             return false, GlorifiedLeveling.i18n.GetPhrase( "glLevelNotHighEnough" )
         end
+    end )
+elseif GlorifiedLeveling.Config.DARKRP_LEVEL_NAME_AT_END then
+    local function AddLevelNames()
+        for k, v in pairs( DarkRPEntities ) do
+            v.label = v.name
+            local valueLevel = v.level or v.Level or v.GlorifiedLeveling_Level
+            if valueLevel then
+                v.label = ( v.label .. " - Level " .. valueLevel )
+            end
+        end
+
+        for k, v in pairs( RPExtraTeams ) do
+            v.label = v.name
+            local valueLevel = v.level or v.Level or v.GlorifiedLeveling_Level
+            if valueLevel then
+                v.label = ( v.label .. " - Level " .. valueLevel )
+            end
+        end
+
+        for k, v in pairs( CustomVehicles ) do
+            v.label = v.name
+            local valueLevel = v.level or v.Level or v.GlorifiedLeveling_Level
+            if valueLevel then
+                v.label = ( v.label .. " - Level " .. valueLevel )
+            end
+        end
+
+        for k, v in pairs( CustomShipments ) do
+            v.label = v.name
+            local valueLevel = v.level or v.Level or v.GlorifiedLeveling_Level
+            if valueLevel then
+                v.label = ( v.label .. " - Level " .. valueLevel )
+            end
+        end
+
+        for k, v in pairs( GAMEMODE.AmmoTypes ) do
+            v.label = v.name
+            local valueLevel = v.level or v.Level or v.GlorifiedLeveling_Level
+            if valueLevel then
+                v.label = ( v.label .. " - Level " .. valueLevel )
+            end
+        end
+    end
+
+    hook.Add( "InitPostEntity", "GlorifiedLeveling.DarkRPIntegration.InitPostEntity", function()
+        AddLevelNames()
+    end ) 
+    
+    hook.Add( "OnReloaded", "GlorifiedLeveling.DarkRPIntegration.OnReloaded", function()
+        AddLevelNames()
     end )
 end
--- lua/glorifiedleveling/sh_glorifiedleveling_config.lua
+++ lua/glorifiedleveling/sh_glorifiedleveling_config.lua
@@ -7,5 +7,6 @@
 
 --[[ Integrations ]]--
     GlorifiedLeveling.Config.SUPPORT_DARKRP = true
+        GlorifiedLeveling.Config.DARKRP_LEVEL_NAME_AT_END = true -- Should we add "- Level x" to the end of job and entity names?
     GlorifiedLeveling.Config.SUPPORT_VENATUSS_HUD_MAKER = false
 --[[ Integrations End ]]--
