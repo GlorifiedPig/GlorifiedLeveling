075fb825c55ed6e5afda208331da794cfcb5b3f8
GlorifiedPig <killingpigs123@gmail.com>
2020-11-20 10:54:43 +0200

    looks cool

--- lua/glorifiedleveling/modules/ui/leaderboard/cl_glorifiedleveling_leaderboard_leaderlist.lua
+++ lua/glorifiedleveling/modules/ui/leaderboard/cl_glorifiedleveling_leaderboard_leaderlist.lua
@@ -59,7 +59,7 @@ end
 
 function PANEL:PerformLayout( w, h )
     self.PositionPos = w / 10
-    self.NamePos = w / 1.8
+    self.NamePos = w / 1.7
     self.XPPos = w / 1.2
     self.LevelPos = w
 
@@ -73,11 +73,6 @@ function PANEL:Paint( w, h )
     local titleBarHeight = h / 12
     draw.RoundedBox( 8, 0, 0, w, h, Color( 73, 73, 73 ) )
     draw.RoundedBoxEx( 8, 0, 0, w, titleBarHeight, Color( 34, 34, 34 ), true, true )
-
-    -- Let's draw some invisible lines just to help visualize where the labels will be placed.
-    draw.RoundedBox( 0, self.PositionPos, 0, 1, h, Color( 255, 255, 255 ) ) -- Place indicator.
-    draw.RoundedBox( 0, self.NamePos, 0, 1, h, Color( 255, 255, 255 ) ) -- Name indicator.
-    draw.RoundedBox( 0, self.XPPos, 0, 1, h, Color( 255, 255, 255 ) ) -- XP indicator.
 
     draw.SimpleText( "#", "GlorifiedLeveling.Leaderboard.LeaderboardTitleBar", self.PositionPos / 2, titleBarHeight / 2, Color( 255, 255, 255, 255 ), TEXT_ALIGN_CENTER, TEXT_ALIGN_CENTER )
     draw.SimpleText( "Name", "GlorifiedLeveling.Leaderboard.LeaderboardTitleBar", ( self.PositionPos + self.NamePos ) / 2, titleBarHeight / 2, Color( 255, 255, 255, 255 ), TEXT_ALIGN_CENTER, TEXT_ALIGN_CENTER )
@@ -97,10 +92,19 @@ function PANEL:Paint( w, h )
             draw.SimpleText( k, "GlorifiedLeveling.Leaderboard.LeaderboardPositionText", self.PositionPos - self.PositionWidth / 2, tblY + tblHeight / 2, Color( 255, 255, 255 ), TEXT_ALIGN_CENTER, TEXT_ALIGN_CENTER )
             local name = string.sub( v.Name, 1, string.len( v.Name ) >= 26 and 26 or string.len( v.Name ) )
             draw.SimpleText( name, "GlorifiedLeveling.Leaderboard.LeaderboardText", self.NamePos - self.NameWidth + 8, tblY + tblHeight / 2, Color( 255, 255, 255 ), TEXT_ALIGN_LEFT, TEXT_ALIGN_CENTER )
+
+            draw.RoundedBox( 4, self.XPPos - self.XPWidth + 8, tblY + 6, self.XPWidth - 16, tblHeight - 12, Color( 89, 175, 63 ) )
+
+            draw.RoundedBox( 4, self.LevelPos - self.LevelWidth + 12, tblY + 6, self.LevelWidth - 24, tblHeight - 12, Color( 89, 175, 63 ) )
         end
     else
         draw.SimpleText( "There is nobody here :(", "DermaDefault", 5, titleBarHeight + 5, Color( 255, 255, 255 ) )
     end
+
+    -- Let's draw some invisible lines just to help visualize where the labels will be placed.
+    draw.RoundedBox( 0, self.PositionPos, 0, 1, h, Color( 255, 255, 255 ) ) -- Place indicator.
+    draw.RoundedBox( 0, self.NamePos, 0, 1, h, Color( 255, 255, 255 ) ) -- Name indicator.
+    draw.RoundedBox( 0, self.XPPos, 0, 1, h, Color( 255, 255, 255 ) ) -- XP indicator.
 end
 
 vgui.Register( "GlorifiedLeveling.Leaderboard.LeaderList", PANEL, "Panel" )
