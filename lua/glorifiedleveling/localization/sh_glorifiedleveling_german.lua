cc9309ad0c16361a1e0d8f9222159ee8940cc304
GlorifiedPig <killingpigs123@gmail.com>
2020-11-28 15:26:25 +0200

    add german

---
+++ lua/glorifiedleveling/localization/sh_glorifiedleveling_german.lua
@@ -0,0 +1,54 @@
+
+GlorifiedLeveling.i18n.RegisterPhrases( "de", {
+    glDarkTheme = "Dunkel",
+    glLevelNotHighEnough = "Dein Level ist zu niedrig dafür.",
+    glYouReceivedXP = "Du hast %s XP bekommen.",
+    glYouReceivedXPKill = "Du hast %s XP für das töten von %s bekommen.",
+    glLevelX = "Level %s",
+    glLevelUp = "Aufgelevelt!",
+    glAdminMenuLockdown = "SPERREN",
+    glAdminMenuPlayers = "SPIELER",
+    glPlayersOnline = "Spieler online: %s",
+    glSetLevel = "Level setzen",
+    glResetLevel = "Level zurücksetzen",
+    glAddXP = "XP hinzufügen",
+    glConfirmation = "Bestätigen",
+    glYes = "Ja",
+    glNo = "Nein",
+    glConfirmationResetLevel = "Willst du das Level diese Spieler zurücksetzen?",
+    glEnterLevel = "Schreibe hier drunter das neue Level des Spielers.",
+    glEnterXP = "Schreibe wie viel XP du hinzufügen willst.",
+    glEnter = "Hinzufügen",
+    glInvalidAmount = "Ungültige Zahl ausgewählt.",
+    glMaxLevel = "Maximales Level",
+
+    -- Leaderboard localization.
+    glLeaderboard = "Bestenliste",
+    glName = "Name",
+    glXP = "XP",
+    glLevel = "Level",
+    glNobodyHere = "Hier ist noch niemand :(",
+
+    -- Perk localization.
+    glSkills = "Fertigkeiten",
+    glPoints = "%s Punkte",
+    glComplete = "Komplett",
+
+    glHasteBoots = "Geschwindigkeits Schuhe",
+    glHasteBootsDescription = "Mehr Geschwindigkeit",
+
+    glFeralLeaping = "Sprungkraft Schuhe",
+    glFeralLeapingDescription = "Spring höher, weniger Fallschaden",
+
+    glIronSkin = "Eisen Rüstung",
+    glIronSkinDescription = "Mehr Rüstung beim Spawnen",
+
+    glJuggernaut = "Starke Haut",
+    glJuggernautDescription = "Mehr Leben beim Spawnen",
+
+    glBulletAbsorption = "Kugel absorbieren",
+    glBulletAbsorptionDescription = "Bekomme weniger Schaden",
+
+    glHighGradeBullets = "Bessere Munition",
+    glHighGradeBulletsDescription = "Mach mehr Schaden",
+} )
