7264650ef2a1f49c54610d454dc55af2d5df7435
GlorifiedPig <killingpigs123@gmail.com>
2020-11-28 15:34:51 +0200

    fix issue with glorifiedbanking + 2.1.1

--- lua/autorun/sh_glorifiedleveling_autoincluder.lua
+++ lua/autorun/sh_glorifiedleveling_autoincluder.lua
@@ -13,7 +13,7 @@ GlorifiedLeveling = GlorifiedLeveling or {
     Perks = {
         Enum = {}
     },
-    Version = "2.1.0"
+    Version = "2.1.1"
 }
 
 print( "[GlorifiedLeveling] This server is running version " .. GlorifiedLeveling.Version .. "." )
