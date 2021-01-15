f6d61f3e0dfd714ccfe1943b82e22515e0abbe28
GlorifiedPig <ryan@glorifiedstudios.com>
2021-01-15 20:07:23 +0200

    fix TTT integration problems

--- lua/autorun/sh_glorifiedleveling_autoincluder.lua
+++ lua/autorun/sh_glorifiedleveling_autoincluder.lua
@@ -13,7 +13,7 @@ GlorifiedLeveling = GlorifiedLeveling or {
     Perks = {
         Enum = {}
     },
-    Version = "2.2.5"
+    Version = "2.2.6"
 }
 
 print( "[GlorifiedLeveling] This server is running version " .. GlorifiedLeveling.Version .. "." )
--- lua/glorifiedleveling/modules/integrations/xpgain/sv_glorifiedleveling_xpgain_ttt.lua
+++ lua/glorifiedleveling/modules/integrations/xpgain/sv_glorifiedleveling_xpgain_ttt.lua
@@ -3,10 +3,18 @@ if not GlorifiedLeveling.Config.XPGain.TTT_MODULE_ENABLED then return end
 
 local moduleSettings = GlorifiedLeveling.Config.XPGain.TTT_MODULE_SETTINGS
 
+local function IsTraitor( ply )
+    return ply.IsTraitor and ply:IsTraitor()
+end
+
+local function IsDetective( ply )
+    return ply.IsDetective and ply:IsDetective()
+end
+
 if moduleSettings.ROUND_WON_REWARD then
     hook.Add( "TTTEndRound", "GlorifiedLeveling.XPGain.TTT.TTTEndRound", function( result )
         for k, v in pairs( player.GetAll() ) do
-            if ( v:IsTraitor() and result == WIN_TRAITOR ) or ( not v:IsTraitor() and result == WIN_INNOCENT ) then
+            if ( IsTraitor( v ) and result == WIN_TRAITOR ) or ( not IsTraitor( v ) and result == WIN_INNOCENT ) then
                 GlorifiedLeveling.AddPlayerXP( ply, moduleSettings.ROUND_WON_AMOUNT )
             end
         end
@@ -14,15 +22,15 @@ if moduleSettings.ROUND_WON_REWARD then
 end
 
 hook.Add( "PlayerDeath", "GlorifiedLeveling.XPGain.TTT.PlayerDeath", function( victim, inflictor, attacker )
-    if moduleSettings.TRAITOR_KILLED_REWARD and not attacker:IsTraitor() and victim:IsTraitor() then
+    if moduleSettings.TRAITOR_KILLED_REWARD and not IsTraitor( attacker ) and IsTraitor( victim ) then
         GlorifiedLeveling.AddPlayerXP( ply, moduleSettings.TRAITOR_KILLED_AMOUNT )
     end
 
-    if moduleSettings.INNOCENT_KILLED_AS_TRAITOR_REWARD and attacker:IsTraitor() and not victim:IsTraitor() and not victim:IsActiveDetective() then
+    if moduleSettings.INNOCENT_KILLED_AS_TRAITOR_REWARD and IsTraitor( attacker ) and not IsTraitor( victim ) and not IsDetective( victim ) then
         GlorifiedLeveling.AddPlayerXP( ply, moduleSettings.INNOCENT_KILLED_AS_TRAITOR_AMOUNT )
     end
 
-    if moduleSettings.DETECTIVE_KILLED_AS_TRAITOR_REWARD and attacker:IsTraitor() and victim:IsActiveDetective() then
+    if moduleSettings.DETECTIVE_KILLED_AS_TRAITOR_REWARD and IsTraitor( attacker ) and IsDetective( victim ) then
         GlorifiedLeveling.AddPlayerXP( ply, moduleSettings.DETECTIVE_KILLED_AS_TRAITOR_AMOUNT )
     end
 end )
